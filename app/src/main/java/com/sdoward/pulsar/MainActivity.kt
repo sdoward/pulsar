1cfc40bd55fd893739911270b460f35b684197cb
Sam Doward <sam.doward@gmail.com>
2021-11-23 22:14:48 +0100

    Improve example

--- app/src/main/java/com/sdoward/pulsar/MainActivity.kt
+++ app/src/main/java/com/sdoward/pulsar/MainActivity.kt
@@ -3,15 +3,22 @@ package com.sdoward.pulsar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sdoward.pulsar.ui.theme.PlusarTheme
 import java.time.Instant
 import java.util.*
+import kotlin.math.exp
 
 
 const val MILLISECONDS_IN_DAY = 86_400_000
@@ -24,17 +31,36 @@ class MainActivity : ComponentActivity() {
             PlusarTheme {
                 Surface(color = MaterialTheme.colors.background) {
                     val valueRange = 0..1000L
-                    val contributions = mutableMapOf<Date, Long>()
+                    val contributions = remember { mutableMapOf<Date, Long>() }
                     (0..364).forEach {
                         val millis = START_OF_2020 * (MILLISECONDS_IN_DAY * it)
                         val date = Date.from(Instant.ofEpochMilli(millis))
                         contributions[date] = valueRange.random()
                     }
-                    PulsarChart(
-                        contributions = contributions,
-                        shape = Shape.Squircle,
-                        color = Color.Green
-                    )
+                    val shapeExpanded = remember {
+                        mutableStateOf(false)
+                    }
+                    val colorExpanded = remember {
+                        mutableStateOf(false)
+                    }
+                    val shape = remember {
+                        mutableStateOf(Shape.Squircle)
+                    }
+                    val color = remember {
+                        mutableStateOf(Color.Blue)
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        ShapeControl(shapeExpanded, shape)
+                        ColorControl(colorExpanded, color)
+                        PulsarChart(
+                            contributions = contributions,
+                            shape = shape.value,
+                            color = color.value
+                        )
+                    }
                 }
             }
         }
@@ -42,14 +68,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun ShapeControl(
+    expanded: MutableState<Boolean>,
+    shape: MutableState<Shape>
+) {
+    Box {
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = { expanded.value = true }) {
+            Text(text = shape.value.name)
+        }
+        DropdownMenu(expanded = expanded.value, onDismissRequest = {}) {
+            Shape.values().forEach {
+                DropdownMenuItem(onClick = {
+                    expanded.value = false
+                    shape.value = it
+                }) {
+                    Text(text = it.name)
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    PlusarTheme {
-        Greeting("Android")
+private fun ColorControl(
+    expanded: MutableState<Boolean>,
+    color: MutableState<Color>
+) {
+    Box {
+        Button(
+            modifier = Modifier.padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = color.value
+            ),
+            onClick = { expanded.value = true }) {
+            Text(text = "Color")
+        }
+        DropdownMenu(expanded = expanded.value, onDismissRequest = {}) {
+            listOf(
+                Color.Black,
+                Color.Red,
+                Color.Blue,
+                Color.Cyan,
+                Color.Green,
+                Color.Magenta,
+                Color.Yellow
+            ).forEach {
+                DropdownMenuItem(onClick = {
+                    expanded.value = false
+                    color.value = it
+                }) {
+                    Box(
+                        modifier = Modifier
+                            .width(64.dp)
+                            .height(32.dp)
+                            .background(it)
+                    )
+                }
+            }
+        }
     }
 }
