50ceffabeca5a787ce03dff3554f65bb94a11461
Sam Doward <sam.doward@gmail.com>
2021-11-27 22:52:04 +0100

    Add alpha and size styles

--- app/src/main/java/com/sdoward/pulsar/MainActivity.kt
+++ app/src/main/java/com/sdoward/pulsar/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -13,13 +12,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sdoward.pulsar.ui.theme.PlusarTheme
 import java.time.Instant
 import java.util.*
-import kotlin.math.exp
-
 
 const val MILLISECONDS_IN_DAY = 86_400_000
 const val START_OF_2020 = 1577836800000
@@ -27,20 +23,23 @@ const val START_OF_2020 = 1577836800000
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val valueRange = 0..1000L
+        val contributions = mutableMapOf<Date, Long>()
+        (0..364).forEach {
+            val millis = START_OF_2020 * (MILLISECONDS_IN_DAY * it)
+            val date = Date.from(Instant.ofEpochMilli(millis))
+            contributions[date] = valueRange.random()
+        }
         setContent {
             PlusarTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    val valueRange = 0..1000L
-                    val contributions = remember { mutableMapOf<Date, Long>() }
-                    (0..364).forEach {
-                        val millis = START_OF_2020 * (MILLISECONDS_IN_DAY * it)
-                        val date = Date.from(Instant.ofEpochMilli(millis))
-                        contributions[date] = valueRange.random()
-                    }
                     val shapeExpanded = remember {
                         mutableStateOf(false)
                     }
                     val colorExpanded = remember {
+                        mutableStateOf(false)
+                    }
+                    val styleExpanded = remember {
                         mutableStateOf(false)
                     }
                     val shape = remember {
@@ -52,17 +51,23 @@ class MainActivity : ComponentActivity() {
                     val padding = remember {
                         mutableStateOf(4F)
                     }
+                    val style = remember {
+                        mutableStateOf(Style.ALPHA)
+                    }
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
                     ) {
                         ShapeControl(shapeExpanded, shape)
                         ColorControl(colorExpanded, color)
-                        PaddingControl(padding = padding)
+                        NumberControl(padding = padding)
+                        StyleControl(styleExpanded, style)
                         PulsarChart(
+                            modifier = Modifier.fillMaxSize(),
                             contributions = contributions,
                             shape = shape.value,
                             color = color.value,
+                            style = style.value,
                             pulsePadding = padding.value.dp
                         )
                     }
@@ -73,13 +78,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun ColumnScope.PaddingControl(padding: MutableState<Float>) {
+private fun ColumnScope.NumberControl(padding: MutableState<Float>) {
     Text(text = "Padding: ${padding.value}")
     Slider(value = padding.value,
         valueRange = 0F..20f,
         onValueChange = {
             padding.value = it
         })
+}
+
+@Composable
+private fun StyleControl(expanded: MutableState<Boolean>, style: MutableState<Style>) {
+    Box {
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = { expanded.value = true }) {
+            Text(text = style.value.name)
+        }
+        DropdownMenu(expanded = expanded.value, onDismissRequest = {}) {
+            Style.values().forEach {
+                DropdownMenuItem(onClick = {
+                    expanded.value = false
+                    style.value = it
+                }) {
+                    Text(text = it.name)
+                }
+            }
+        }
+    }
 }
 
 @Composable
--- pulsar/src/main/java/com/sdoward/pulsar/pulsar.kt
+++ pulsar/src/main/java/com/sdoward/pulsar/pulsar.kt
@@ -2,22 +2,18 @@ package com.sdoward.pulsar
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.inset
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import java.time.Instant
 import java.util.*
 import kotlin.math.ceil
-import kotlin.math.max
 import kotlin.math.min
 
 const val MILLISECONDS_IN_DAY = 86_400_000
@@ -25,7 +21,7 @@ const val START_OF_2020 = 1577836800000
 
 @Preview
 @Composable
-fun PulsarChartPreview() {
+internal fun PulsarChartPreview() {
     val valueRange = 0..1000L
     val contributions = mutableMapOf<Date, Long>()
     (0..364).forEach {
@@ -38,8 +34,9 @@ fun PulsarChartPreview() {
             .fillMaxSize()
             .padding(8.dp),
         contributions = contributions,
-        shape = Shape.Square,
+        shape = Shape.Circle,
         color = Color.Green,
+        style = Style.ALPHA_SIZE,
         pulsePadding = 0.dp
     )
 }
@@ -50,6 +47,7 @@ fun PulsarChart(
     contributions: Map<Date, Long>,
     shape: Shape = Shape.Square,
     color: Color = Color.Red,
+    style: Style = Style.ALPHA,
     pulsePadding: Dp = 4.dp
 ) {
     val earliestDate = contributions.keys.minOf { it }
@@ -66,16 +64,16 @@ fun PulsarChart(
         color = color,
         rowCount = 7,
         rowStart = dayOfWeek,
-        alphas = alphas,
+        values = alphas,
         shape = shape,
+        style = style,
         pulsePadding = pulsePadding
     )
 }
 
 @Preview
 @Composable
-fun PulsarCorePreview() {
-    //   val alphas = (0..364).map { 1F }
+internal fun PulsarCorePreview() {
     val alphas = listOf(
         0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
         0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
@@ -88,9 +86,11 @@ fun PulsarCorePreview() {
             .fillMaxSize()
             .padding(9.dp),
         color = Color.Red,
+        shape = Shape.Circle,
+        style = Style.SIZE,
         rowCount = 7,
         rowStart = 0,
-        alphas = alphas
+        values = alphas
     )
 }
 
@@ -101,13 +101,14 @@ fun PulsarCore(
     shape: Shape = Shape.Squircle,
     rowCount: Int = 7,
     rowStart: Int = 0,
-    alphas: List<Float>,
+    style: Style = Style.ALPHA,
+    values: List<Float>,
     pulsePadding: Dp = 4.dp
 ) {
     if (rowStart >= rowCount) {
         throw IllegalStateException("RowStart cannot be higher than RowCount. RowStart: $rowStart RowCount: $rowCount")
     }
-    val columnCount = ceil(alphas.size.toFloat() / rowCount.toFloat())
+    val columnCount = ceil(values.size.toFloat() / rowCount.toFloat())
     Canvas(modifier = modifier) {
         val columnWidth = (size.width / columnCount) - pulsePadding.value
         val rowWidth = (size.height / rowCount) - pulsePadding.value
@@ -121,11 +122,16 @@ fun PulsarCore(
             pulsePadding.value / 2,
             y
         )
-        alphas.forEach { alpha ->
+        values.forEach { value ->
+            val alpha = when (style) {
+                Style.ALPHA -> value
+                Style.SIZE -> 1F
+                Style.ALPHA_SIZE -> value
+            }
             when (shape) {
-                Shape.Circle -> drawCirclePulse(color, offset, boxSize, alpha)
-                Shape.Square -> drawSquarePulse(color, offset, boxSize, alpha)
-                Shape.Squircle -> drawSquirclePulse(color, offset, boxSize, alpha)
+                Shape.Circle -> drawCirclePulse(color, offset, boxSize, style, alpha, value)
+                Shape.Square -> drawSquarePulse(color, offset, boxSize, style, alpha, value)
+                Shape.Squircle -> drawSquirclePulse(color, offset, boxSize, style, alpha, value)
             }
             currentRow++
             offset += Offset(0F, boxSize.height + pulsePadding.value)
@@ -139,31 +145,69 @@ fun PulsarCore(
     }
 }
 
-private fun DrawScope.drawSquarePulse(color: Color, offset: Offset, size: Size, alpha: Float) {
+private fun DrawScope.drawSquarePulse(
+    color: Color,
+    offset: Offset,
+    size: Size,
+    style: Style,
+    alpha: Float,
+    pulseValue: Float,
+) {
+    val rectifiedSize = when (style) {
+        Style.ALPHA -> size
+        Style.SIZE -> size * pulseValue
+        Style.ALPHA_SIZE -> size * pulseValue
+    }
+    val sizeDelta = (size.height - rectifiedSize.height) / 2
     drawRect(
         color = color,
-        topLeft = offset,
+        topLeft = offset + Offset(sizeDelta, sizeDelta),
         alpha = alpha,
-        size = size
+        size = rectifiedSize
     )
 }
 
-private fun DrawScope.drawSquirclePulse(color: Color, offset: Offset, size: Size, alpha: Float) {
+private fun DrawScope.drawSquirclePulse(
+    color: Color,
+    offset: Offset,
+    size: Size,
+    style: Style,
+    alpha: Float,
+    pulseValue: Float
+) {
+    val rectifiedSize = when (style) {
+        Style.ALPHA -> size
+        Style.SIZE -> size * pulseValue
+        Style.ALPHA_SIZE -> size * pulseValue
+    }
+    val sizeDelta = (size.height - rectifiedSize.height) / 2
     drawRoundRect(
         color = color,
-        topLeft = offset,
+        topLeft = offset + Offset(sizeDelta, sizeDelta),
         alpha = alpha,
-        size = size,
-        cornerRadius = CornerRadius(size.width / 8F, size.height / 8F)
+        size = rectifiedSize,
+        cornerRadius = CornerRadius(rectifiedSize.width / 8F, rectifiedSize.height / 8F)
     )
 }
 
-private fun DrawScope.drawCirclePulse(color: Color, offset: Offset, size: Size, alpha: Float) {
+private fun DrawScope.drawCirclePulse(
+    color: Color,
+    offset: Offset,
+    size: Size,
+    style: Style,
+    alpha: Float,
+    pulseValue: Float
+) {
+    val radius = when (style) {
+        Style.ALPHA -> size.height / 2F
+        Style.SIZE -> (size.height / 2F) * pulseValue
+        Style.ALPHA_SIZE -> (size.height / 2F) * pulseValue
+    }
     drawCircle(
         color = color,
         center = offset + size.center,
         alpha = alpha,
-        radius = size.height / 2F
+        radius = radius
     )
 }
 
@@ -171,4 +215,10 @@ enum class Shape {
     Circle,
     Square,
     Squircle
+}
+
+enum class Style {
+    ALPHA,
+    SIZE,
+    ALPHA_SIZE
 }
